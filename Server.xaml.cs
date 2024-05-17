7f84cce3b875aa443da5524a147b8c1809a9bc2d
Shaibmen <ibin2005636@gmail.com>
2024-05-18 00:11:20 +0300

    3

--- Server.xaml.cs
+++ Server.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,7 +23,6 @@ namespace Server_app
     /// </summary>
     public partial class Server : Window
     {
-        private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         private Socket soket;
         private List<Socket> clients = new List<Socket>();
         private List<string> userNames = new List<string>();
@@ -36,82 +36,119 @@ namespace Server_app
             soket.Bind(ipPoint);
             soket.Listen(1000);
 
-            Task.Run(() => ListenToClients(cancellationTokenSource.Token), cancellationTokenSource.Token);
+            ListenToClients();
 
         }
 
-        private async Task ListenToClients(CancellationToken cancellationToken)
+        private async Task ListenToClients()
         {
-            while(!cancellationToken.IsCancellationRequested)
+            while (true)
             {
                 var client = await soket.AcceptAsync();
                 clients.Add(client);
-                string userNamesString = string.Join("#", userNames);
-                await SendName(client, userNamesString);
 
-                Task.Run(() => RecieveMessage(client, cancellationToken), cancellationToken);
-            
+                RecieveMessage(client);
             }
         }
 
-        private async Task RecieveMessage(Socket client, CancellationToken cancellationToken)
+        private async Task HandleClientDisconnect(Socket client)
         {
-            while(!cancellationToken.IsCancellationRequested)
+            try
             {
-                byte[] byritos = new byte[1024];
-                int BytesRecirverd = await client.ReceiveAsync(byritos);
-                string message = Encoding.UTF8.GetString(byritos);
+                await client.ReceiveAsync(new ArraySegment<byte>(new byte[0]), SocketFlags.None);
+            }
+            catch (SocketException)
+            {
+                // соединение было прервано 
+            }
+            DisconnectClient(client);
+            client.Close();
+        }
 
-                if (message.Contains("#"))
+        private async Task RecieveMessage(Socket client)
+        {
+            try
+            {
+                while (true)
                 {
-                    string name = message.Split('#')[1];
-
-                    if (!userNames.Contains(name))
+                    byte[] byritos = new byte[1024];
+                    int byresReceived = await client.ReceiveAsync(byritos);
+                    if (byresReceived == 0)
                     {
-                        userNames.Add(name);
+                        await HandleClientDisconnect(client);
+                        break;
+                    }
+                    string message = Encoding.UTF8.GetString(byritos);
+
+                    if (message.Contains("#"))
+                    {
+                        string name = message.Split('#')[1];
+
+                        if (!userNames.Contains(name))
+                        {
+                            userNames.Add(name);
+                            UsersLbx.Items.Add(name);
+
+                            // отправляем список всех пользователей новому участнику
+                            foreach (var item in clients)
+                            {
+                                if (item == client) // cокет нового участника
+                                {
+                                    SendName(item, "#" + string.Join("#", userNames));
+                                }
+                            }
+
+                            //для старых участников #не забыть
+                            string newUserMessage = $"#{name}";
+                            MessagesLbx.Items.Add(newUserMessage);
+                            foreach (var item in clients)
+                            {
+                                if (item != client)
+                                {
+                                    SendMessage(item, newUserMessage);
+                                }
+                            }
+                        }
+
+                        //cловарик для дисконекта ip зачение name ключ
+                        IPEndPoint remoteEndPoint = (IPEndPoint)client.RemoteEndPoint;
+                        string clientIp = remoteEndPoint.Address.ToString();
+                        clientIpToName[clientIp] = name;
                     }
 
-                    UsersLbx.Items.Add(name);
-                    foreach (var item in clients)
+                    else if (message.Contains("/disconnect"))
                     {
-                        SendName(item, $"#{name}");
+                        // заменяем вызов DisconnectClient(client) на HandleClientDisconnect(client) 
+                        await HandleClientDisconnect(client);
+                        break;
                     }
-
-                    //cловарик для дисконекта ip зачение name ключ
-                    IPEndPoint remoteEndPoint = (IPEndPoint)client.RemoteEndPoint;
-                    string clientIp = remoteEndPoint.Address.ToString();
-                    clientIpToName[clientIp] = name;
-                }
-                if (message.Contains("/disconnect"))
-                {
-                    DisconnectClient(client);
-                }
-                else
-                {
-                    MessagesLbx.Items.Add(message);
-                    foreach (var item in clients)
+                    else
                     {
-                        SendMessage(item, message);
+                        MessagesLbx.Items.Add(message);
+                        foreach (var item in clients)
+                        {
+                            SendMessage(item, message);
+                        }
                     }
                 }
             }
-        }
+            catch (OperationCanceledException)
+            {
 
-        private async Task SendMessage(Socket client, string message)
-        {
-            byte[] bytes = Encoding.UTF8.GetBytes(message);
-            await client.SendAsync(bytes, SocketFlags.None);
-        }
-        
-        private async Task SendName(Socket client, string name)
-        {
-            byte[] bytes = Encoding.UTF8.GetBytes(name);
-            await client.SendAsync(bytes, SocketFlags.None);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
         }
 
         private async Task DisconnectClient(Socket client)
         {
-            
+
             IPEndPoint remoteEndPoint = (IPEndPoint)client.RemoteEndPoint;
             string clientIp = remoteEndPoint.Address.ToString();
 
@@ -128,7 +165,7 @@ namespace Server_app
                 userNames.Remove(userName);
                 UsersLbx.Items.Remove(userName);
 
-                
+
                 string message = $"{userName} отключился.";
                 MessagesLbx.Items.Add(message);
                 foreach (var item in clients)
@@ -138,13 +175,22 @@ namespace Server_app
             }
 
             client.Shutdown(SocketShutdown.Both);
-            client.Close();
         }
-        public void CloseServer()
+
+
+        private async Task SendMessage(Socket client, string message)
         {
-            cancellationTokenSource.Cancel();
-            soket.Close();
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            await client.SendAsync(bytes, SocketFlags.None);
         }
+        
+        private async Task SendName(Socket client, string name)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(name);
+            await client.SendAsync(bytes, SocketFlags.None);
+        }
+
+        
     }
 }
     
